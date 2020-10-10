e8e099d70e62fc94daeb5e994abef2aebba42fed
nk <snmnka001@myuct.ac.za>
2020-10-10 19:21:43 +0200

    Observation Class getInfo getObservation and update methods created

--- gym_molecule/envs/molecule_env.py
+++ gym_molecule/envs/molecule_env.py
@@ -98,3 +98,33 @@ class Action():
         self.pos       = pos 
         self.query     = query
         self.isSmarts  = isSmarts        
+class Observation:
+    
+    def __init__(self, mol):
+        self.mol = mol
+        self.observation = Chem.MolToSmiles(mol)
+        self.info = []
+    
+   
+    def getInfo(self):
+        self.info.clear()
+        feats = factory.GetFeaturesForMol(self.mol)
+        fp = AllChem.GetMorganFingerprintAsBitVect(self.mol,2,nBits=1024)
+        
+        for y in feats:
+            self.info.append(y.GetType())
+        
+        DataStructs.ConvertToNumpyArray(fp,fp_arr)
+        self.bits = np.nonzero(fp_arr)   
+        return self.bits,self.info
+
+    
+    def getObservation(self):
+        self.observation = Chem.MolToSmiles(self.mol)
+        return self.observation
+        
+    def update(self,mol):
+        self.mol = mol 
+
+    
+    
