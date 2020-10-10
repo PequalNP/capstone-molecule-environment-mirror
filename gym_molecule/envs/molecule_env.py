520c968c51972b64c665b79fdbecdbbaaeaa9a0e
nk <snmnka001@myuct.ac.za>
2020-10-10 19:18:17 +0200

    Action class wraps action related variables

--- gym_molecule/envs/molecule_env.py
+++ gym_molecule/envs/molecule_env.py
@@ -84,3 +84,17 @@ class MoleculeEnvironment(gym.Env):
             # can only remove query ie cannot but add
             return False
         
+class Action():
+    def __init__(self):
+        self.action_c = ''
+        self.pos = ''   #front or back
+        self.mol = ''
+        self.query = ''
+        self.isSmarts  = False
+
+    def setAction(self,action,pos='front',query='',mol='C',isSmarts=False): #mol
+        self.action_c  = action
+        self.mol       = mol
+        self.pos       = pos 
+        self.query     = query
+        self.isSmarts  = isSmarts        
