30596565edd47610ff17f0f94d033faa19fe2c1f
nk <snmnka001@myuct.ac.za>
2020-10-10 19:08:00 +0200

    reset method added same constructor

--- gym_molecule/envs/molecule_env.py
+++ gym_molecule/envs/molecule_env.py
@@ -14,11 +14,31 @@ class MoleculeEnvironment(gym.Env):
         self.smiles = []
         
     def step(self):
-        raise NotImplementedError
+        action    = action_ob.action_c.lower()
+        position  = action_ob.pos
+        mol       = action_ob.mol
+        query     = action_ob.query 
+        
+        if (isinstance(action_ob.query,np.ndarray)):  
+            self._queryStep(action,position,mol,query)
+        else :
+            self._simpleStep(action,position,mol)
+        self.current_molecule = RWMol(Chem.MolFromSmiles(self._listToSmiles()))  
+        
+        self.obs.update(self.current_molecule) 
+        self.mol_Steps.append(self.current_molecule)
+        legend = str(len(self.mol_Steps))+ ". " + Chem.MolToSmiles(self.current_molecule)
+        self.smiles.append(legend) 
+        return self.obs    
 
     def reset(self):
-        raise NotImplementedError
-
+        default_smile = 'C'
+        self.current_molecule  = RWMol(Chem.MolFromSmiles(default_smile))  
+        self.obs = Observation(self.current_molecule)
+        self.molecule_list = [Mol_Feature(default_smile)]
+        self.mol_Steps =[]
+        self.smiles = []
+        
     def render(self):
         raise NotImplementedError
 
