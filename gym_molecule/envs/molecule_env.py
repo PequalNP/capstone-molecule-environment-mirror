bc31b3375ce263e309abcc1393df70806eedf558
nk <snmnka001@myuct.ac.za>
2020-10-10 19:01:15 +0200

    MoleculeEnvirnment constructor initializing Current molecule, observation molecule list

--- gym_molecule/envs/molecule_env.py
+++ gym_molecule/envs/molecule_env.py
@@ -6,7 +6,13 @@ from gym.utils import seeding
 class MoleculeEnvironment(gym.Env):
     def __init__(self):
         super().__init__()
-
+        default_smile = 'C'
+        self.current_molecule  = RWMol(Chem.MolFromSmiles(default_smile))  
+        self.obs = Observation(self.current_molecule)
+        self.molecule_list = [Mol_Feature(default_smile)]
+        self.mol_Steps =[]
+        self.smiles = []
+        
     def step(self):
         raise NotImplementedError
 
