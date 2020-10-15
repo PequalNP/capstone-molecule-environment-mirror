01108c3032fd44cd77ab716a99f39992dcaa3dd6
nk <snmnka001@myuct.ac.za>
2020-10-15 14:42:46 +0200

    Wrote test for Obeservation getInfo method

--- tests/test_observation.py
+++ tests/test_observation.py
@@ -1,6 +1,8 @@
 import unittest
 from .context import observation
 from rdkit import Chem
+from rdkit.Chem import AllChem
+from rdkit import DataStructs
 from rdkit.Chem import ChemicalFeatures
 from rdkit.Chem import RWMol
 from rdkit import RDConfig
@@ -11,18 +13,26 @@ factory = ChemicalFeatures.BuildFeatureFactory(fdefName)
 class TestObservation(unittest.TestCase):
 
     def setUp(self):
-        mol = RWMol(Chem.MolFromSmiles("C"))
-        self.obs = observation.Observation(mol)
+        self.mol = RWMol(Chem.MolFromSmiles("C"))
+        self.obs = observation.Observation(self.mol)
 
     def tearDown(self):
         pass
     
     def test_getInfo(self):
-        #To-Do
-        #Update the test
-        features = factory.GetFeaturesForMol(self.obs.mol)
+        info = []
+        bits = []
+        feats = factory.GetFeaturesForMol(self.mol)
+        fp = AllChem.GetMorganFingerprintAsBitVect(self.mol,2,nBits = 1024)
+         
+        for y in feats:
+            info.append(y.GetType())
+        fp_arr = np.zeros((1,))
+        DataStructs.ConvertToNumpyArray(fp,fp_arr)
+        bits = np.nonzero(fp_arr)   
+       
 
-        self.assertEqual(self.obs.getInfo(), features)
+        self.assertEqual(self.obs.getInfo(), (bits,info))
 
     def test_getObservation(self):
         observations = Chem.MolToSmiles(self.obs.mol)
